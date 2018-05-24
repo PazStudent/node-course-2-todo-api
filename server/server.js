20c51b2cb7509f19f3529fba9815c31122731fc5
pazjalgames <pazjalgames@gmail.com>
2018-05-24 20:52:33 +0300

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+
+const {ObjectID} = require('mongodb');
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -31,6 +33,26 @@ app.get('/todos' , (req , res) => {
 
   });
 });
+
+app.get('/todos/:id' , (req , res) => {
+  var id = req.params.id;
+  if(!ObjectID.isValid(id)){
+    console.log('ID not valid.');
+    return res.status(404).send();
+  }
+  Todo.findById(id).then((todo) => {
+    if(!todo){
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+
+
+});
+
+
 
 app.listen(3000 , () => {
   console.log('Started server on port 3000');
