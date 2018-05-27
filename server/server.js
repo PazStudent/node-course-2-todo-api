942087fdca7a29db8436f13d6aa45d333b28fcf5
pazjalgames <pazjalgames@gmail.com>
2018-05-27 12:48:51 +0300

    Add test cases for GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -37,8 +37,7 @@ app.get('/todos' , (req , res) => {
 app.get('/todos/:id' , (req , res) => {
   var id = req.params.id;
   if(!ObjectID.isValid(id)){
-    console.log('ID not valid.');
-    return res.status(404).send();
+      return res.status(404).send();
   }
   Todo.findById(id).then((todo) => {
     if(!todo){
