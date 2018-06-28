88ff7c048b5aef677336a6af3f281b5b74ee5622
Paz Jaldety <paz@coperato.local>
2018-06-28 10:23:08 +0300

    Add get users/me and authentication middleware

--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ const bodyParser = require('body-parser');
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo.js');
 var {User} = require('./models/user.js');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT;
@@ -123,6 +124,11 @@ app.post('/users' , (req , res) => {
   })
 });
 
+
+
+app.get('/users/me' , authenticate , (req, res) => {
+  res.send(req.user);
+});
 
 app.listen(port , () => {
   console.log(`Started server on port ${port}`);
