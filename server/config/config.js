7dc1c9ce823c642cb12396f31b7028796c30b862
Paz Jaldety <paz@coperato.local>
2018-07-02 10:57:45 +0300

    Update config to use JSON file

--- server/config/config.js
+++ server/config/config.js
@@ -1,13 +1,14 @@
 var env = process.env.NODE_ENV  || 'development';
-if(env === 'development'){
-  console.log('Dev - Mode');
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://127.0.0.1:27017/TodoApp';
-}else if(env ==='test'){
-  console.log('Test - Mode');
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://127.0.0.1:27017/TodoAppTest';
-}else{
+
+if(env === 'development' || env === 'test'){
+  var config = require('./config.json');
+  var env_config = config[env];
+
+  Object.keys(env_config).forEach((key) =>{
+    process.env[key] = env_config[key];
+  });
+}
+else{
   console.log('Production - Mode');
   process.env.MONGODB_URI = 'mongodb://app:app123@ds137650.mlab.com:37650/todo-app';
 }
